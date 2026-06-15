a42c108fde4e27f0475f08a4d1704f72f72ffcdc
DazzleAvalon <macmenigallb50217@masseyhigh.school.nz>
2026-06-15 13:19:25 +1200

    updated

--- 01_conversion.py
+++ 01_conversion.py
@@ -10,7 +10,7 @@ def instructions():
 Welcome to the Ultimate conversion calculator!
 - Please enter a unit you want converted from and to
 - Mass (eg: 50 g to kg) unit options: mg, g, kg, T
-- Time (eg: 300 s to h) unit options: ms, s, m, h, d, month, y 
+- Time (eg: 300 s to h) unit options: ms, s, min, h, d, month, y 
 - Distance (eg: 500 mm to m) unit options: mm, cm, m, km
 
 -Please no cross converting units
@@ -21,16 +21,18 @@ To exit the program, just type "xxx"
 
 # Main routine goes here
 
+statement_generator("The ultimate conversion calculator" ,"-")
+
 # Display instructions if requested
 want_instructions = input("Press <enter> to read the instructions " 
                           "or any key to continue ")
 
 if want_instructions == "":
     instructions()
-
 def num_check(question):
     error = "you chose to continue\n"
     while True:
+
         response = input(question).lower
         if response == "xxx":
             return response
@@ -50,7 +52,7 @@ distance_dict = {
 time_dict = {
     "ms": 3600 * 1000,
     "s": 3600,
-    "m": 60,
+    "min": 60,
     "h": 1,
     "d": 1 / 24,
     "month": 1 / 168,
@@ -60,7 +62,7 @@ mass_dict= {
     "mg": 1000,
     "g": 1,
     "kg": .001,
-    "T": .00001,
+    "T": .000001,
 }
 # combine for checking
 all_units = {**distance_dict,**time_dict,**mass_dict}
@@ -69,29 +71,28 @@ all_units = {**distance_dict,**time_dict,**mass_dict}
 def get_category(unit):
     if unit in distance_dict:
         return "distance"
-    if unit in time_dict:
+    elif unit in time_dict:
         return "time"
-    if unit in mass_dict:
+    elif unit in mass_dict:
         return "mass"
     else:
         return None
-
 # Get amount and units (assume they are valid)
 amount = float(input("how much? "))
 from_unit = input("From unit? ")
 to_unit = input("To unit? ")
 
 # Multiply to get to our standard value...
-multiply_by = mass_dict[to_unit]
+multiply_by = all_units[from_unit]
 standard = amount * multiply_by
 
 # Divide to get our desired value
-divide_by = mass_dict[from_unit]
+divide_by = all_units[to_unit]
 answer = standard / divide_by
 if (from_unit in distance_dict and to_unit in distance_dict or
         from_unit in mass_dict and to_unit in mass_dict or
         from_unit in time_dict and to_unit in time_dict):
-    print(f"There are answer {to_unit} in {amount} {from_unit}")
+    print(f"There are {answer} {to_unit} in {amount} {from_unit}")
     print()
 
 # check if units are valid
@@ -104,10 +105,12 @@ else:
         print(f"Cannot convert {from_unit}({from_cat}) to {to_unit}({to_cat})")
         print()
 
+
 while True:
     to_convert = num_check("End?: ")
+    print("You chose to end, I hope you enjoyed")
 
-
-
+    if to_convert == "xxx":
+        break
 
 print(f"Thank you for using the ultimate conversion calculator!")
